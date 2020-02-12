8b6b5293b34ee48d4087de7cbe8aa8a4f66d86c8
steelybel <bel.bradberry26@gmail.com>
2020-02-12 12:42:02 -0800

    First commit

---
+++ AI for Games - Bel/Assets/BelDumbAI.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BelDumbAI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform chasing;
+    [SerializeField] private float maxVelocity = 3.0f;
+    private Vector3 currentVelocity;
+    [SerializeField] private float wanderRadius = 1f;
+    [SerializeField] private float wanderDist = 1f;
+    [SerializeField] private float wanderJitter = 1f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Wander();
+        //Seek(chasing);
+    }
+    private void AddForce(Vector3 f)
+    {
+        currentVelocity += f * Time.deltaTime;
+    }
+    void Seek(Transform chase)
+    {
+        Vector3 v = (chase.position - transform.position).normalized * maxVelocity;
+        Vector3 force = v - currentVelocity;
+        AddForce(force);
+        transform.position += (currentVelocity * Time.deltaTime);
+        transform.rotation = Quaternion.LookRotation(v);
+    }
+    void Flee(Transform run)
+    {
+        Vector3 v = (transform.position - run.position).normalized * maxVelocity;
+        Vector3 force = v - currentVelocity;
+        AddForce(force);
+        transform.position += (currentVelocity * Time.deltaTime);
+        transform.rotation = Quaternion.LookRotation(v);
+    }
+    void Wander()
+    {
+        Vector3 w = new Vector3(Random.Range(-wanderJitter, wanderJitter),0, Random.Range(-wanderJitter, wanderJitter));
+        w = w.normalized * wanderDist;
+        w += transform.position;
+        Vector3 force = w - currentVelocity;
+        AddForce(force);
+        transform.position += (currentVelocity * Time.deltaTime);
+        transform.rotation = Quaternion.LookRotation(w);
+    }
+    void Pursue()
+    {
+        Vector3 v = (chasing.transform.position - transform.position).normalized * maxVelocity;
+        Vector3 force = v - currentVelocity;
+        currentVelocity += force * Time.deltaTime;
+        transform.Translate(currentVelocity * Time.deltaTime);
+        transform.rotation = Quaternion.LookRotation(v);
+    }
+}
