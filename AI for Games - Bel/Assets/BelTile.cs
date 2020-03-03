a0756d03d769422f7e2ecc7f3c5a6304a62a41a8
steelybel <bel.bradberry26@gmail.com>
2020-03-02 17:51:21 -0800

    Working through tilemapping

---
+++ AI for Games - Bel/Assets/BelTile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BelTile : MonoBehaviour
+{
+    public int gScore;
+    public Vector2 pos;
+    public BelTile prevNode = null;
+    public BelTile up, down, right, left = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Report()
+    {
+        Debug.Log("The node above costs " + up.gScore);
+        Debug.Log("The node below costs " + down.gScore);
+        Debug.Log("The node left costs " + left.gScore);
+        Debug.Log("The node right costs " + right.gScore);
+    }
+}
---
+++ AI for Games - Bel/Assets/BelTilemap.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BelTilemap : MonoBehaviour
+{
+    public BelTile tileT;
+    public int width = 10;
+    public int height = 10;
+    public BelTile[,] belTiles;
+    private Vector2 look = Vector2.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        belTiles = new BelTile[width, height];
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                belTiles[x, y] = Instantiate(tileT, transform.position + new Vector3(x, y, 0) - new Vector3(width / 2, height / 2, 0), Quaternion.identity, transform);
+                belTiles[x, y].pos = new Vector2(x, y);
+                belTiles[x, y].gScore = 1;
+            }
+        }
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                if (y > 0) { belTiles[x, y].down = belTiles[x, y - 1]; }
+                if (y < height) { belTiles[x, y].up = belTiles[x, y + 1]; }
+                if (x > 0) { belTiles[x, y].left = belTiles[x - 1, y]; }
+                if (x < width) { belTiles[x, y].right = belTiles[x + 1, y]; }
+            }
+        }
+        belTiles[0, 0].gScore = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
