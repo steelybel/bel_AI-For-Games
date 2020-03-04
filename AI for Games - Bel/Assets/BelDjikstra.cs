91eb018b974053438c05508ac9972b033d2df233
steelybel <bel.bradberry26@gmail.com>
2020-03-03 17:52:24 -0800

    Completed Djikstra exercise

---
+++ AI for Games - Bel/Assets/BelDjikstra.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(BelTilemap))]
+public class BelDjikstra : MonoBehaviour
+{
+    private BelTilemap map;
+    public Material blank;
+    public Material mark;
+    public Material path;
+    public Vector2Int start = new Vector2Int(0, 0);
+    public Vector2Int goal = new Vector2Int(1, 1);
+    public List<BelTile> openList = new List<BelTile>();
+    public List<BelTile> closedList = new List<BelTile>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        map = GetComponent<BelTilemap>();
+        foreach (BelTile node in map.nodes)
+        {
+            node.GetComponent<MeshRenderer>().material = blank;
+        }
+        openList.Add(map.nodes[start.x,start.y]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (!closedList.Contains(map.nodes[goal.x,goal.y]))
+        {
+            while (openList.Count > 0)
+            {
+                Djikstra(openList[0]);
+            }
+        }
+        
+
+    }
+    bool Found()
+    {
+        return false;
+        //BelTile g = map.nodes[(int)goal.x, (int)goal.y];
+        //if (current.up == g || current.down == g || current.left == g || current.right == g) { return true; }
+        //else { return false; }
+    }
+    void Djikstra(BelTile current)
+    {
+        openList.Remove(current);
+        MarkNode(current);
+        closedList.Add(current);
+        List<BelTile> connected = new List<BelTile>();
+        if (current.up != null && !closedList.Contains(current.up) && !openList.Contains(current.up)) { current.up.prevNode = current; connected.Add(current.up); }
+        if (current.down != null && !closedList.Contains(current.down) && !openList.Contains(current.down)) { current.down.prevNode = current; connected.Add(current.down); }
+        if (current.left != null && !closedList.Contains(current.left) && !openList.Contains(current.left)) { current.left.prevNode = current; connected.Add(current.left); }
+        if (current.right != null && !closedList.Contains(current.right) && !openList.Contains(current.right)) { current.right.prevNode = current; connected.Add(current.right); }
+        TileComp tc = new TileComp();
+        if (connected.Count > 0) { connected.Sort(0, connected.Count - 1, tc); }
+        connected.Reverse();
+        openList.AddRange(connected);
+        if (current == map.nodes[goal.x,goal.y])
+        {
+            TraceBack(current);
+        }
+    }
+    void AStar(BelTile current)
+    {
+        openList.Remove(current);
+        MarkNode(current);
+        closedList.Add(current);
+        List<BelTile> connected = new List<BelTile>();
+        if (current.up != null && !closedList.Contains(current.up) && !openList.Contains(current.up)) { current.up.prevNode = current; connected.Add(current.up); }
+        if (current.down != null && !closedList.Contains(current.down) && !openList.Contains(current.down)) { current.down.prevNode = current; connected.Add(current.down); }
+        if (current.left != null && !closedList.Contains(current.left) && !openList.Contains(current.left)) { current.left.prevNode = current; connected.Add(current.left); }
+        if (current.right != null && !closedList.Contains(current.right) && !openList.Contains(current.right)) { current.right.prevNode = current; connected.Add(current.right); }
+        TileComp tc = new TileComp();
+        if (connected.Count > 0) { connected.Sort(0, connected.Count - 1, tc); }
+        connected.Reverse();
+        openList.AddRange(connected);
+        if (current == map.nodes[goal.x, goal.y])
+        {
+            TraceBack(current);
+        }
+    }
+    void MarkNode(BelTile t)
+    {
+        t.GetComponent<MeshRenderer>().material = mark;
+    }
+    void MarkNodeAlt(BelTile t)
+    {
+        t.GetComponent<MeshRenderer>().material = path;
+    }
+    void TraceBack(BelTile a)
+    {
+        BelTile track = a;
+        while (track.prevNode != null)
+        {
+            MarkNodeAlt(track);
+            track = track.prevNode;
+        }
+    }
+}
+
+public class TileComp : IComparer<BelTile>
+{
+    // Compares by Height, Length, and Width.
+    public int Compare(BelTile x, BelTile y)
+    {
+        if (x.gScore().CompareTo(y.gScore()) != 0)
+        {
+            return x.gScore().CompareTo(y.gScore());
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
--- AI for Games - Bel/Assets/BelTile.cs
+++ AI for Games - Bel/Assets/BelTile.cs
@@ -2,28 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider))]
 public class BelTile : MonoBehaviour
 {
-    public int gScore;
+    public int weight = 1;
     public Vector2 pos;
     public BelTile prevNode = null;
     public BelTile up, down, right, left = null;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Ray rayUp = new Ray(transform.position, Vector3.up);
+        Ray rayDown = new Ray(transform.position, Vector3.down);
+        Ray rayLeft = new Ray(transform.position, Vector3.left);
+        Ray rayRight = new Ray(transform.position, Vector3.right);
+        RaycastHit hit;
+        if (Physics.Raycast(rayUp, out hit))
+        {
+            if (hit.collider.tag.Equals("Node"))
+            {
+                up = hit.collider.gameObject.GetComponent<BelTile>();
+            }
+        }
+        if (Physics.Raycast(rayDown, out hit))
+        {
+            if (hit.collider.tag.Equals("Node"))
+            {
+                down = hit.collider.gameObject.GetComponent<BelTile>();
+            }
+        }
+        if (Physics.Raycast(rayLeft, out hit))
+        {
+            if (hit.collider.tag.Equals("Node"))
+            {
+                left = hit.collider.gameObject.GetComponent<BelTile>();
+            }
+        }
+        if (Physics.Raycast(rayRight, out hit))
+        {
+            if (hit.collider.tag.Equals("Node"))
+            {
+                right = hit.collider.gameObject.GetComponent<BelTile>();
+            }
+        }
     }
     public void Report()
     {
-        Debug.Log("The node above costs " + up.gScore);
-        Debug.Log("The node below costs " + down.gScore);
-        Debug.Log("The node left costs " + left.gScore);
-        Debug.Log("The node right costs " + right.gScore);
+        Debug.Log("The node above costs " + up.gScore());
+        Debug.Log("The node below costs " + down.gScore());
+        Debug.Log("The node left costs " + left.gScore());
+        Debug.Log("The node right costs " + right.gScore());
+    }
+    public float gScore()
+    {
+        if (prevNode != null)
+        {
+            float butt = prevNode.gScore() + weight;
+            return butt;
+        }
+        else
+        {
+            return 0;
+        }
+        
+    }
+    public float hScore()
+    {
+        return 0;
+    }
+    public float fScore()
+    {
+        return (gScore()+hScore());
     }
 }
